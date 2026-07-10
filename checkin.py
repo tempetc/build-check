e337e0c1a364352258ecd24f7e44f882547a0bc1
TEmper <tempui@outlook.com>
2026-07-10 20:46:05 +0800

    Update checkin.py

--- checkin.py
+++ checkin.py
@@ -104,7 +104,7 @@ class Config:
     DEFAULT_VERBOSE = False
 
     """默认域名"""
-    DOMAINS = ["glados.cloud"]
+    DOMAINS = ["glados.cloud","railgun.info"]
 
     """兑换计划列表"""
     EXCHANGE_PLANS = {
