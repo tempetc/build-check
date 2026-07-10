18983921143ffe0750706c3341ead00173a125ed
TEmper <tempui@outlook.com>
2026-07-10 13:54:22 +0800

    Update checkin.py

--- checkin.py
+++ checkin.py
@@ -5,6 +5,7 @@ import logging
 from enum import Enum
 from typing import Dict, List, Optional, Tuple, Union
 from dataclasses import dataclass, asdict
+from pypushdeer import PushDeer
 from logging_config import init_logger
 
 
@@ -93,6 +94,7 @@ def log_method(func):
 class Config:
     """应用配置"""
 
+    ENV_PUSH_KEY = "PUSHDEER_SENDKEY"
     ENV_COOKIES = "GLADOS_COOKIES"
     ENV_EXCHANGE_PLAN = "GLADOS_EXCHANGE_PLAN"
     ENV_VERBOSE = "GLADOS_VERBOSE"
@@ -114,6 +116,7 @@ class Config:
     }
 
     def __init__(self):
+        self.push_key: str = ""
         self.cookies_list: List[str] = []
         self.exchange_plan: str = self.DEFAULT_EXCHANGE_PLAN
         self.verbose: bool = self.DEFAULT_VERBOSE
@@ -121,10 +124,16 @@ class Config:
 
     def _load_config(self) -> None:
         """加载配置"""
+        push_key_env: Optional[str] = os.environ.get(self.ENV_PUSH_KEY)
         raw_cookies_env: Optional[str] = os.environ.get(self.ENV_COOKIES)
         exchange_plan_env: Optional[str] = os.environ.get(self.ENV_EXCHANGE_PLAN)
         verbose_env: Optional[str] = os.environ.get(self.ENV_VERBOSE)
 
+        if not push_key_env:
+            logger.warning(f"{LogEmoji.WARNING} 环境变量 '{self.ENV_PUSH_KEY}' 未设置。")
+            self.push_key = ""
+        else:
+            self.push_key = push_key_env
 
         if not raw_cookies_env:
             logger.warning(f"{LogEmoji.WARNING} 环境变量 '{self.ENV_COOKIES}' 未设置。")
@@ -146,6 +155,7 @@ class Config:
                 self.exchange_plan = self.DEFAULT_EXCHANGE_PLAN
 
         logger.info(f"{LogEmoji.INFO} 共加载了 {len(self.cookies_list)} 个 Cookie 用于签到。")
+        logger.info(f"{LogEmoji.INFO} 当前 {self.ENV_PUSH_KEY} {'已设置' if push_key_env else '未设置'}。")
         logger.info(f"{LogEmoji.INFO} 当前 {self.ENV_EXCHANGE_PLAN}: {self.exchange_plan}。")
 
         if verbose_env is not None:
@@ -379,6 +389,29 @@ class CheckinResult:
         result_dict = asdict(self)
         return result_dict
 
+
+class PushService:
+    """推送服务"""
+
+    def __init__(self, config: Config):
+        self.config = config
+
+    def send(self, title: str, content: str) -> bool:
+        """发送推送"""
+        if not self.config.push_key:
+            logger.info(f"{LogEmoji.WARNING} 未设置推送密钥，跳过推送通知。")
+            return False
+
+        try:
+            pushdeer = PushDeer(pushkey=self.config.push_key)
+            pushdeer.send_text(title, desp=content)
+            logger.info(f"{LogEmoji.SUCCESS} 推送通知发送成功。")
+            return True
+        except Exception as e:
+            logger.error(f"{LogEmoji.ERROR} 发送推送通知失败: {e}")
+            return False
+
+
 class Checker:
     """签到"""
 
@@ -417,7 +450,6 @@ class Checker:
                         result_message = f"结果: {result.status}, 获得 {result.points} 积分, 剩余 {result.days}, 总 {result.points_total}, {result.exchange}"
                     self._log(cookie_idx, domain, LogEmoji.SUCCESS, result_message, force=True)
                 else:
-                        # result_message = f"结果: {result.status}, 获得 {result.points} 积分, 剩余 {result.days}, 总 {result.points_total}, {result.exchange}"
                     self._log(cookie_idx, domain, LogEmoji.WARNING, result_message, force=True)
 
     def _checkin_on_domain(self, cookie: str, cookie_idx: int, domain: str) -> CheckinResult:
@@ -482,6 +514,7 @@ class Checker:
         log_content = "\n".join(log_content_lines)
         return title, content, log_content
 
+
 # 初始化日志
 logger = init_logger()
 
@@ -511,6 +544,10 @@ def main():
         logger.error(f"{LogEmoji.ERROR} 主程序执行过程中发生未预期的错误: {e}")
         title, content, log_content = "# 脚本执行出错", str(e), str(e)
 
+    # 4. 发送推送
+    logger.info(f"{LogEmoji.START} 步骤 4: 发送推送")
+    push_service = PushService(config if "config" in locals() else "")
+    push_service.send(title, content)
     logger.info(f"{LogEmoji.END} 签到完成")
 
 
